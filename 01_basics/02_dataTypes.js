38e595671760cc95321a1a328ec1ec74419d4793
[shubhamsingh74] <shubhamsingh74970@gmail.com>
2024-02-18 00:35:38 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as never version
+
+// alert(3+2)  // we are using nodejs , not browser
+
+console.log(10+100)
+console.log("Shubham singh")
+
+let name = "shubham"
+let age = 21
+let isLoggedIn = false
+
+console.table([
+    name,age,isLoggedIn
+])
+
+
+//number => 2 to power 53
+// bigint 
+//string =>""
+//boolean => true/false
+//null  => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+
+//object 
+
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,58 @@
+"use strict"
+
+let nameMan = "shubham "
+let age = 21
+
+console.log(typeof nameMan)
+console.log(typeof (nameMan))
+console.log(typeof (age))
+
+let changeNameDataType = Number(nameMan)
+let changeAgeDataType = String(age)
+
+
+console.log(typeof changeNameDataType)
+console.log(changeNameDataType)
+console.log(typeof changeAgeDataType)
+console.log(changeAgeDataType)
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "shubham" => true
+
+
+
+
+
+
+
+//*************************** Operations *********************************/
+
+let value = 7
+let negValue =-value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(5%4)
+
+
+let str1 ="hello shubham"
+let str2 = " Singh"
+
+let str3 = str1+str2
+console.log(str3)
+console.log("shubham");
+console.log("shubham\nsingh");
